41d0c9b45957ab595acf65e6d11cc7ea875d1dc4
ForestDuck <105928922+ForestDuck@users.noreply.github.com>
2023-01-31 14:38:33 +0200

    Update Arrays.js

--- Lesson_3_Arrays/Arrays.js
+++ Lesson_3_Arrays/Arrays.js
@@ -1,3 +1,4 @@
+//EX-1
 const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
 let temp = userNames[0];
 userNames[0] = userNames[1];
@@ -6,12 +7,9 @@ let initials=userNames.map(function(userName) {
 return userName.split(' ').map(word => word.charAt(0)).join('.')
 } );
 
-
-
-
 console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
-
+//EX-2
 const currentMaxValue = 4589;
 let reverseMaxValue;
 
@@ -20,6 +18,7 @@ reverseMaxValue = Number(currentMaxValue.toString().split('').sort().reverse().j
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
 
+//EX-3
 const resultsArray = [1, 2, [3, [4]]];
 let productOfArray;
 
