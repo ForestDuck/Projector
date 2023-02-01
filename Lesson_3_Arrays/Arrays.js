aa2989947107bbae8bd42a11e5947b9e5131ae02
ForestDuck <105928922+ForestDuck@users.noreply.github.com>
2023-02-01 09:24:29 +0200

    Update Arrays.js
    Changes according to task

--- Lesson_3_Arrays/Arrays.js
+++ Lesson_3_Arrays/Arrays.js
@@ -1,9 +1,7 @@
 //EX-1
 const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let temp = userNames[0];
-userNames[0] = userNames[1];
-userNames[1] = temp;
-let initials=userNames.map(function(userName) {
+
+let initials=userNames.sort().map(function(userName) {
 return userName.split(' ').map(word => word.charAt(0)).join('.')
 } );
 
@@ -13,7 +11,7 @@ console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 const currentMaxValue = 4589;
 let reverseMaxValue;
 
-reverseMaxValue = Number(currentMaxValue.toString().split('').sort().reverse().join(''));
+reverseMaxValue = Number(currentMaxValue.toString().split('').reverse().join(''));
 
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
